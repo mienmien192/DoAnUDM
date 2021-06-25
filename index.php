58c0c77711ed5d7a7e32c86a802e1db1e075b91f
Hạ Trần Miên Khánh <mienmien192@gmail.com>
2021-06-25 10:07:26 +0700

    chao chao

--- index.php
+++ index.php
@@ -4,3 +4,15 @@ require_once './mvc/Bridge.php';
 $x=new App();
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello moi nguoi em la chao day
+</body>
+</html>
